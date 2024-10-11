24776784f7515b13ccee7dc347dcf2aa93ef38a2
VictorGG2416 <mapachose@gmail.com>
2024-10-11 16:56:29 -0600

    Create Listas.c

---
+++ Listas.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define N 50
+typedef struct nodo
+{
+	char nom[50];
+	struct nodo *sig;
+}TNodo;
+
+void leeDatos(FILE *fp, TNodo **cab);
+TNodo *CreaNodo(char d[N]);
+void insertaInicio(TNodo **cab, char d[N]);
+void imprime(TNodo *cab);
+void libera(TNodo **cab);
+
+
+
+
+int main () 
+{
+	FILE *fp;
+	TNodo *cab;
+	cab=NULL;
+	char nom[N];
+	
+	printf("Nombre del archivo: ");
+	scanf("%s",nom);
+	
+	if((fp=fopen(nom,"r"))==NULL)
+	{
+		printf("Archibo no encontrado :(");
+		exit(0);
+	}
+	printf("**********DATOS DEL ARCHIVO**********");
+	leeDatos(fp,&cab);
+	imprime(cab);
+	
+	libera(&cab);
+	fclose(fp);
+}
+
+void leeDatos(FILE *fp, TNodo **cab)
+{
+	char nom[N];
+	
+	while(!feof(fp))
+	{
+		fgets(nom,N,fp);
+		insertaInicio(cab,nom);
+	}
+}
+
+void insertaInicio(TNodo **cab, char d[N])
+{
+	TNodo *nodo=CreaNodo(d);
+	if(*cab == NULL)
+		*cab=nodo;
+	else
+	{
+		nodo->sig = *cab;
+		*cab=nodo;
+	}
+}
+
+TNodo *CreaNodo(char d[N])
+{
+	TNodo *nodo=(TNodo *)malloc(sizeof(TNodo));
+	
+	if(nodo)
+	{
+		nodo->nom[0]='\0';
+		strcpy(nodo->nom,d);
+		nodo->sig=NULL;
+		return nodo;
+	}
+	printf("Error :(");
+	exit(0);	
+}
+
+void imprime(TNodo *cab)
+{
+	TNodo *ptr=cab;
+	while(ptr)
+	{
+		printf("%s\n",ptr->nom);
+		ptr=ptr->sig;
+	}
+}
+
+void libera(TNodo **cab)
+{
+	TNodo *ptr;
+	while(*cab)
+	{
+		ptr= *cab;
+		*cab=ptr->sig;
+		free(ptr);
+	}
+}
