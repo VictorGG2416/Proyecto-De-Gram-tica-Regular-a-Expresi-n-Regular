1425ed9a9cb4201af1fa71f33266041912b50a31
VictorGG2416 <mapachose@gmail.com>
2024-10-23 16:38:59 +0000

    23/10/2024

--- main.c
+++ main.c
@@ -1,21 +1,4 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define N 50
-typedef struct nodo
-{
-	char nom[50];
-	struct nodo *sig;
-}TNodo;
-
-void leeDatos(FILE *fp, TNodo **cab);
-TNodo *CreaNodo(char d[N]);
-void insertaInicio(TNodo **cab, char d[N]);
-void imprime(TNodo *cab);
-void libera(TNodo **cab);
-
-
-
+#include "main.h"
 
 int main () 
 {
